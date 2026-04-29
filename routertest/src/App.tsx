8c74f086af98d7375c240a9d38ad6d00539b552d
yeji <chlrhhan@gmail.com>
2026-04-29 16:51:41 +0900

    router study

---
+++ routertest/src/App.tsx
@@ -0,0 +1,53 @@
+import "./App.css";
+import { Header, Main, Product, NotFound } from "./Components/Components";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Main /> },
+      { path: "product/*", element: <Product /> },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+  {
+    // 이런식으로 따로 user만 해서 빼서 쓸 수 있다. 
+    path:'/user',
+    element:<Layout/>,
+    children:
+    [
+      {index:true, element:<Main/>}
+    ]
+  },
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
