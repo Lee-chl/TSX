6cddf6a6fa5db6dcc089ad7867238b9f3f9ec57c
yeji <chlrhhan@gmail.com>
2026-05-07 15:35:31 +0900

    reduxtoolkit

--- reduxtest/src/store.ts
+++ reduxtest/src/store.ts
@@ -1,4 +1,18 @@
-import reducer from "./reducer";
-import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-export const store = createStore(reducer);
+// 여기서 뽑아 사용하니 여기에 다 모아둔다
+const slice = createSlice({
+  name: "counter",
+  initialState: { value: 0 },
+  reducers: {
+    increase: (state) => {
+      state.value += 1;
+    },
+    decrease: (state) => {
+      state.value -= 1;
+    },
+  },
+});
+
+export const { increase, decrease } = slice.actions;
+export const store = configureStore({reducer:slice.reducer})
